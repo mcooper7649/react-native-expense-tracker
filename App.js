8d3c59e365ff77ad7db9a0ca86c023a58ec9180e
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2023-01-29 19:22:00 -0500

    added Context API, configured ADD, UPDATE, and DELETE logic, Configured ExpenseContext Provider Dispatches

--- App.js
+++ App.js
@@ -63,13 +63,24 @@ export default function App() {
     <>
       <StatusBar style="auto" />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+            headerTintColor: 'white',
+          }}
+        >
           <Stack.Screen
             name="ExpensesOverview"
             component={ExpensesOverview}
             options={{ headerShown: false }}
           />
-          <Stack.Screen name="ManageExpense" component={ManageExpenses} />
+          <Stack.Screen
+            name="ManageExpense"
+            component={ManageExpenses}
+            options={{
+              presentation: 'modal',
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
